ffedce72ad916e708ca7cf1308a22cdea12480eb
Eric Melz <eric@emelz.com>
2017-07-01 16:57:36 -0700

    form processing

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.forms import CategoryForm
 from rango.models import Category
 from rango.models import Page
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
 
 
 def show_category(request, category_name_slug):
